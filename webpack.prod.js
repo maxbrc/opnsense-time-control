db34b5dae2981ec27d3c618bcd241a8043f34577
maxbrc <max@biaq.de>
2024-12-15 20:10:27 +0100

    Fix my stupidity

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 import { merge } from "webpack-merge";
 import commonConfig from "./webpack.common.js";
 import { fileURLToPath } from 'url';
+import path from "path";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
