7e013284bc7f4de8e5da024a2b8fdb91fd0ecd94
maxbrc <max@biaq.de>
2024-12-15 20:09:31 +0100

    Add relative path in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,13 @@
 import { merge } from "webpack-merge";
 import commonConfig from "./webpack.common.js";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = merge(commonConfig, {
     mode: "production",
     output: {
-        path: "./client/dist",
+        path: path.join(__dirname,"client/dist"),
         filename: "index_bundle_[contenthash].js"
     }
 })
